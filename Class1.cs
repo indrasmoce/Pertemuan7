7a2b74a65057da836f8049fe2e5d2ceb23f5b8b7
Indra Bagus Setiawan <48389065+indrasmoce@users.noreply.github.com>
2019-05-14 14:49:10 +0700

    Add files via upload
    Update Program Pertemuan 7

---
+++ Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bagu2
+{
+    public class Printer
+    {
+        public String Name { get; set; }
+        public virtual void Print()
+        {
+            Console.WriteLine("Sedang Mencetak ");
+        }
+        public virtual void Show()
+        {
+            Console.WriteLine("Dimensi Yang ditunjukan ");
+        }
+    }
+    public class Epson : Printer
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Epson Display Dimension =  10*11");
+            
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Epson Printer Printing...");
+        }
+    }
+    public class Canon : Printer
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Canon Display Dimension =  9.5*12");
+            
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Canon Printer Printing...");
+        }
+    }
+    public class LaserJet : Printer
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Laser Jet  Display Dimension =  12*12");
+            
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Laser Jet Printer Printing...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bagu2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer();
+            Console.WriteLine("Pilih Printer : ");
+            Console.WriteLine("1. Epson  ");
+            Console.WriteLine("2. Canon  ");
+            Console.WriteLine("3. Laser Jet  ");
+            Console.Write("Nomor Pirnter [1..3] : ");
+            int nomoPrinter = Convert.ToInt32(Console.ReadLine());
+            if (nomoPrinter == 1)
+            {
+                printer = new Epson();
+            }
+            else if (nomoPrinter == 2)
+            {
+                printer = new Canon();
+
+            }
+            else
+                printer = new LaserJet();
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
